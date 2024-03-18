f2beda2e31916fc180f05018d7d77285647ffd2e
Palo23 <lmarespinortizz23@gmail.com>
2024-03-18 13:08:20 -0600

    refactor: add api service

---
+++ utils/api.ts
@@ -0,0 +1,38 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    );
+
+interface PhotoDataTypes {
+    name: string,
+    url: string,
+    date: string
+}
+
+const api = {
+    postPhoto: async (data: PhotoDataTypes) => {
+        const response = await supabase
+            .from('photos')
+            .insert([data]);
+        return response;
+    },
+
+    getPhotos: async () => {
+        const { data, error } = await supabase
+            .from('photos')
+            .select('*');
+        return data;
+    },
+
+    getPhotoByName: async (name: string) => {
+        const { data, error } = await supabase
+            .from('photos')
+            .select('*')
+            .eq('name', name);
+        return data;
+    }
+}
+
+export default api;
