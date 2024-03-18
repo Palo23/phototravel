c144daea51946fd50b011a0306d68093c3e4f1c7
DeadBryam <Bryamu.u@gmail.com>
2024-03-18 14:10:03 -0600

    refactor(photos): add auto animate

--- pages/photos.tsx
+++ pages/photos.tsx
@@ -1,12 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import api from "../utils/api";
 import Head from 'next/head';
 import moment from "moment";
 import { PhotoDataTypes } from "../interfaces";
+import autoAnimate from '@formkit/auto-animate'
 
 const Photos = () => {
+    const parent = useRef(null)
     const [allPhotos, setAllPhotos] = useState([]);
 
+    // Functions
     const getPhotos = async () => {
         const response = await api.getPhotos();
         if (response) setAllPhotos(response);
@@ -25,10 +28,15 @@ const Photos = () => {
         setAllPhotos((prev) => [photo, ...prev]);
     }
 
+    // Hooks
     useEffect(()=>{
         suscribeToPhotos();
         getPhotos();
     }, []);
+
+    useEffect(() => {
+        parent.current && autoAnimate(parent.current)
+      }, [parent])
 
     return (
         <>
@@ -39,7 +47,7 @@ const Photos = () => {
             </Head>
             <div className="p-4">
                 <h1 className="text-4xl mb-4">Album de fotos</h1>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4" ref={parent}>
                     {allPhotos?.map((photo: any, index: number) => (
                         <div key={index} className="relative group overflow-hidden">
                             <img className="w-full h-full object-cover transform group-hover:scale-105 transition-transform duration-300" src={photo.url} alt={photo.name} />
